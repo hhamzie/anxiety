c90288a2bbbe2153c6152ec1954d29c886ffbf52
griec <tgh8wp@virginia.edu>
2024-12-05 04:10:30 -0500

    added Game Over Scene

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,78 +1,84 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using TMPro; // Import TextMeshPro namespace
+using UnityEngine.SceneManagement;
 
-public class Timer : MonoBehaviour, IPointerClickHandler // Implement IPointerClickHandler for event handling
+
+public class Timer : MonoBehaviour
 {
     [SerializeField] private Image uiFill;
-    [SerializeField] private TextMeshProUGUI uiText; // Use TextMeshProUGUI instead of Text
+    [SerializeField] private TextMeshProUGUI uiText;
     public int Duration;
 
-    private bool Pause;
-    private int remainingDuration;
+    public GameObject boy; // Reference to Boy sprite
+    public GameObject girl; // Reference to Girl sprite
+    public string gameOverSceneName = "GameOver"; // Name of the GameOver scene
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        Pause = !Pause;
-    }
+    private int remainingDuration;
 
     void Start()
     {
-        // Ensure the sorting order is set to 1
-        //SetSortingOrder(1);
-
+        InitializeUI();
         Begin(Duration);
     }
 
-    private void Begin(int second)
+    private void InitializeUI()
     {
-        remainingDuration = second;
+        remainingDuration = Duration;
+        uiText.text = $"{remainingDuration / 60:00} : {remainingDuration % 60:00}";
+        uiFill.fillAmount = 1; // Start with a full circle
+    }
+
+    private void Begin(int seconds)
+    {
+        remainingDuration = seconds;
         StartCoroutine(UpdateTimer());
     }
 
     private IEnumerator UpdateTimer()
     {
-        while (remainingDuration >= 0)
+        while (remainingDuration > 0)
         {
-            if (!Pause)
-            {
-                // Update the TextMeshProUGUI text
-                uiText.text = $"{remainingDuration / 60:00} : {remainingDuration % 60:00}";
-                // Update the fill amount of the UI image
-                uiFill.fillAmount = Mathf.InverseLerp(0, Duration, remainingDuration);
-                remainingDuration--;
-                yield return new WaitForSeconds(1f);
-            }
-            yield return null;
+            // Update the UI each second
+            uiText.text = $"{remainingDuration / 60:00} : {remainingDuration % 60:00}";
+            uiFill.fillAmount = Mathf.InverseLerp(0, Duration, remainingDuration);
+
+            remainingDuration--;
+            yield return new WaitForSeconds(1f); // Wait for 1 second
         }
+
+        // Timer reaches 0, switch to GameOver scene
         OnEnd();
     }
 
     private void OnEnd()
+{
+    Debug.Log("Timer ended. Switching to GameOver scene...");
+
+    // Get the Y positions of both players
+    float boyYPosition = boy.transform.position.y;
+    float girlYPosition = girl.transform.position.y;
+
+    // Determine the winner and store it in PlayerPrefs
+    if (boyYPosition > girlYPosition)
     {
-        Debug.Log("End");
+        Debug.Log("Boy wins!");
+        PlayerPrefs.SetString("Winner", "Boy Wins!");
+    }
+    else if (girlYPosition > boyYPosition)
+    {
+        Debug.Log("Girl wins!");
+        PlayerPrefs.SetString("Winner", "Girl Wins!");
+    }
+    else
+    {
+        Debug.Log("It's a tie!");
+        PlayerPrefs.SetString("Winner", "It's a Tie!");
     }
 
-    private void SetSortingOrder(int order)
-    {
-        // Get or add a Canvas component to the GameObject
-        Canvas canvas = GetComponent<Canvas>();
-        if (canvas == null)
-        {
-            canvas = gameObject.AddComponent<Canvas>();
-        }
+    // Switch to the GameOver scene
+    SceneManager.LoadScene(gameOverSceneName);
+}
 
-        // Set the sorting order
-        canvas.overrideSorting = true;
-        canvas.sortingOrder = order;
-
-        // Ensure a CanvasRenderer exists for proper rendering
-        if (GetComponent<CanvasRenderer>() == null)
-        {
-            gameObject.AddComponent<CanvasRenderer>();
-        }
-    }
 }
